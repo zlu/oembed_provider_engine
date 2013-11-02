642ac762206dc0b20bcd12c32ab78f6e409c8e6b
zlu <zlu@me.com>
2013-11-02 09:37:54 +0800

    fixed circular dependency - rails4 complains about class loading

--- app/controllers/oembed_provider_engine/oembed_provider_controller.rb
+++ app/controllers/oembed_provider_engine/oembed_provider_controller.rb
@@ -2,7 +2,7 @@ module OembedProviderEngine
   class OembedProviderController < ApplicationController
     # Prevents the following error from showing up, common in Rails engines
     # A copy of ApplicationController has been removed from the module tree but is still active!
-    unloadable
+    #unloadable
 
     # GET /oembed?url=... json by default
     # GET /oembed.json?url=...
